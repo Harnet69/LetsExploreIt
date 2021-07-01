81d459babb206f611c7da414f04536a79466ca9f
kokhanau <vitalk@yanosik.pl>
2021-07-01 13:51:16 +0200

    Implemented Google maps

--- app/src/main/java/com/harnet69/letsexploreit/features/exploringMap/ExploringMapFragment.kt
+++ app/src/main/java/com/harnet69/letsexploreit/features/exploringMap/ExploringMapFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.harnet69.letsexploreit.R
 import com.harnet69.letsexploreit.databinding.ExploringMapBinding
 
 class ExploringMapFragment : Fragment() {
@@ -15,26 +19,22 @@ class ExploringMapFragment : Fragment() {
     private lateinit var homeViewModel: ExploringMapViewModel
     private var _binding: ExploringMapBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        homeViewModel =
-            ViewModelProvider(this).get(ExploringMapViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        homeViewModel = ViewModelProvider(this).get(ExploringMapViewModel::class.java)
 
         _binding = ExploringMapBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
-        return root
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+
+        return binding.root
     }
 
     override fun onDestroyView() {
